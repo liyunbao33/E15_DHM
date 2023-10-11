6a5594ce1a7486807af5ac966534fde27a90586b
liyun <hanyuntaocn@163.com>
2023-10-11 10:14:37 +0800

    2023.10.11 10:14

--- DHM_ert_rtw/DHM.c
+++ DHM_ert_rtw/DHM.c
@@ -3,9 +3,9 @@
  *
  * Code generated for Simulink model 'DHM'.
  *
- * Model version                  : 1.97
+ * Model version                  : 1.103
  * Simulink Coder version         : 9.7 (R2022a) 13-Nov-2021
- * C/C++ source code generated on : Tue Oct 10 16:15:49 2023
+ * C/C++ source code generated on : Wed Oct 11 10:10:50 2023
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: Intel->x86-64 (Windows64)
@@ -1459,8 +1459,8 @@ void DHM_Step(void)                    /* Explicit Task: DHM_Step */
    *  Inport: '<Root>/VeINP_HWA_Voltage_100mV_VeINP_HWA_Voltage_100mV'
    */
   DHM_FLDoorHndDriver(DHM_U.VeINP_HWA_Voltage_100mV_VeINP_H,
-                      SI_m_RLDoorHndPosSts, DHM_B.SO_b_HndUnfoldReq_l,
-                      DHM_B.SO_b_HndFoldReq_m, &SO_b_MotorA_i, &SO_b_MotorB_b,
+                      SI_m_RLDoorHndPosSts, DHM_B.SO_b_HndFoldReq_m,
+                      DHM_B.SO_b_HndUnfoldReq_l, &SO_b_MotorA_i, &SO_b_MotorB_b,
                       &DHM_B.SO_e_MotorCmd_o, &DHM_B.SO_e_MotorPwm_h,
                       &SO_b_Error_g, &DHM_DW.sf_RLDoorHndDriver);
 
@@ -1489,8 +1489,8 @@ void DHM_Step(void)                    /* Explicit Task: DHM_Step */
    *  Inport: '<Root>/VeINP_HWA_Voltage_100mV_VeINP_HWA_Voltage_100mV'
    */
   DHM_FLDoorHndDriver(DHM_U.VeINP_HWA_Voltage_100mV_VeINP_H,
-                      SI_m_FRDoorHndPosSts, DHM_B.SO_b_HndUnfoldReq_p,
-                      DHM_B.SO_b_HndFoldReq_o, &SO_b_MotorA_i, &SO_b_MotorB_b,
+                      SI_m_FRDoorHndPosSts, DHM_B.SO_b_HndFoldReq_o,
+                      DHM_B.SO_b_HndUnfoldReq_p, &SO_b_MotorA_i, &SO_b_MotorB_b,
                       &DHM_B.SO_e_MotorCmd_m, &DHM_B.SO_e_MotorPwm_c,
                       &SO_b_Error_g, &DHM_DW.sf_FRDoorHndDriver);
 
@@ -1519,8 +1519,8 @@ void DHM_Step(void)                    /* Explicit Task: DHM_Step */
    *  Inport: '<Root>/VeINP_HWA_Voltage_100mV_VeINP_HWA_Voltage_100mV'
    */
   DHM_FLDoorHndDriver(DHM_U.VeINP_HWA_Voltage_100mV_VeINP_H,
-                      SI_m_RRDoorHndPosSts, DHM_B.SO_b_HndUnfoldReq,
-                      DHM_B.SO_b_HndFoldReq, &SO_b_MotorA_i, &SO_b_MotorB_b,
+                      SI_m_RRDoorHndPosSts, DHM_B.SO_b_HndFoldReq,
+                      DHM_B.SO_b_HndUnfoldReq, &SO_b_MotorA_i, &SO_b_MotorB_b,
                       &DHM_B.SO_e_MotorCmd, &DHM_B.SO_e_MotorPwm, &SO_b_Error_g,
                       &DHM_DW.sf_RRDoorHndDriver);
 
